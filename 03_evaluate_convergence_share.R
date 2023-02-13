928448f3a44562d687b654097b3c70a8b13a1fb0
mirkkajones <81167765+mirkkajones@users.noreply.github.com>
2023-02-13 16:10:37 +0200

    Update 03_evaluate_convergence_share.R

--- 03_evaluate_convergence_share.R
+++ 03_evaluate_convergence_share.R
@@ -1,19 +1,19 @@
 #################################################################################################################################################
-#Codes associated to the analysis used the paper 
-#"The legacy of human use in Amazonian palm communities along environmental and accessibility gradients". 
-#Global Ecology and Biogeography. In press.
-#Codes by Otso Ovaskainen, Mirkka Jones and Gabriela Zuquim
-#The codes were modified from November 2020 Hmsc course scripts. 
-#Current versions of these training materials are available at https://www.helsinki.fi/en/researchgroups/statistical-ecology/software/hmscCodes
-#see readme file for detail
+# Codes associated with the analyses presented in the paper 
+# "The legacy of human use in Amazonian palm communities along environmental and accessibility gradients". 
+# Global Ecology and Biogeography. In press.
+# Codes by Otso Ovaskainen, Mirkka Jones and Gabriela Zuquim
+# The codes were modified from November 2020 Hmsc course scripts prepared by Otso Ovaskainen, Jari Oksanen and others.
+# Current versions of these training materials are available at https://www.helsinki.fi/en/researchgroups/statistical-ecology/software/hmscCodes
+# see readme file for details.
 ##################################################################################################################################################
 
 library(Hmsc)
-install.packages("colorspace")
+# If necessary: install.packages("colorspace")
 library(colorspace)
-install.packages("vioplot")
+# If necessary: install.packages("vioplot")
 library("vioplot")
-install.packages("sm")
+# If necessary: install.packages("sm")
 library(sm)
 
 load("models_thin_1000_samples_250_chains_4.Rdata")
