5accf756b39ac5f66c5726544d1c98b539877986
mirkkajones <81167765+mirkkajones@users.noreply.github.com>
2023-02-13 16:13:09 +0200

    Update and rename 05_generatesANDplot_VarPart_values_share.r to 05_generate_plot_VarPart_values_share.r

--- 05_generate_plot_VarPart_values_share.r
+++ 05_generate_plot_VarPart_values_share.r
@@ -1,4 +1,12 @@
-#METADATA
+#################################################################################################################################################
+# Codes associated with the analyses presented in the paper 
+# "The legacy of human use in Amazonian palm communities along environmental and accessibility gradients". 
+# Global Ecology and Biogeography. In press.
+# Codes by Otso Ovaskainen, Mirkka Jones and Gabriela Zuquim
+# The codes were modified from November 2020 Hmsc course scripts prepared by Otso Ovaskainen, Jari Oksanen and others.
+# Current versions of these training materials are available at https://www.helsinki.fi/en/researchgroups/statistical-ecology/software/hmscCodes
+# see readme file for details.
+##################################################################################################################################################
 
 library(Hmsc)
 load("models_thin_1000_samples_250_chains_4.Rdata")
