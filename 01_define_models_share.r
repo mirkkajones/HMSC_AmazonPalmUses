a2610d9864d6a6ee613d4f3ce69f193bb573c1f1
mirkkajones <81167765+mirkkajones@users.noreply.github.com>
2023-05-25 07:57:27 +0300

    Update 01_define_models_share.r

--- 01_define_models_share.r
+++ 01_define_models_share.r
@@ -15,8 +15,6 @@ rm(Y_Juvenile)
 
 # Check for absent (0) or ubiquitous species (1) in the species data matrices.
 range(colMeans(Y_Adult>0))
-min(colSums(Y_Adult>0))
-# =0.
 
 #Adult Y and Tr data preparation:
 # Remove rare species (10 or less observations)
@@ -26,24 +24,17 @@ Y_Adult = Y_Adult[,-rarespecies]
 
 # Species richness distribution (adults only) across sites.
 Tr_Adult = droplevels(Tr[row.names(Tr) %in% colnames(Y_Adult),])
-Tr_Adult[, 9:10] = apply(Tr_Adult[, 9:10], 2, as.numeric)
+Tr_Adult = Tr_Adult[, 1:8]
 
 TrFormula = ~human_food + material + medicine + use_intensity
 
 # prepare environmetal data
-library(vegan)
-pcoa = cmdscale(vegdist(1*(Y_Adult>0), binary = TRUE), k = 3, eig = TRUE)
-pca_clim = prcomp(X[, 2:19], scale = TRUE)
+X$hab_type2 = as.character(X$hab_type)
+X$hab_type2[X$hab_type2=="White sand"] = "Terra firma"
+X$hab_type2 = factor(X$hab_type2)
+X$log.HAND_50 = log10(X$HAND_50+2)
 
-# Correlation table sorted by correlations with taxa PCoA axis 1:
-cortable = round(cor(data.frame(pcoa$points[, 1], pcoa$points[, 2], pcoa$points[, 3], pca_clim$x[, 1:3], X[, -29]), use = "complete.obs"), digits = 3)[, 1:3]# Correlation table sorted by correlations with taxa PCoA axis 1:
-head(cortable[order(cortable[, 1]), ]);tail(cortable[order(cortable[, 1]), ])
-head(cortable[order(cortable[, 2]), ]);tail(cortable[order(cortable[, 2]), ])
-
-# Based on PCoA correlations, selected two Chelsa climatic variables quantifying variation in temperature and rainfall for inclusion as fixed effects in 
-# the Hmsc models, as well as three Landsat satellite reflectance bands, log(soil cations), HAND, log(distance to river) and habitat type.
-X = X[, c("Chelsa150_bio6", "Chelsa150_bio15", "Band2_median15", "Band4_median15", "Band7_median15", "logCat", "HAND_50", "DistRivlog10", "hab_type")]
-relevel(X$hab_type, ref = "Terra firma")
+X = X[, c("Chelsa150_bio6", "Chelsa150_bio15", "Band3_median15", "Band4_median15", "Band7_median15", "logCat", "log.HAND_50", "DistRivlog10", "hab_type2")]
 
 XFormula = ~Chelsa150_bio6 + Chelsa150_bio15 + Band3_median15 + Band4_median15 + Band7_median15 + poly(logCat, 2, raw = TRUE) + log.HAND_50 + DistRivlog10 + hab_type2
 
@@ -60,13 +51,12 @@ Ypa_AD = 1*(Y_Adult>0)
 #####################################################################
 m1 = Hmsc(Y=Ypa_AD, XData = X,  XFormula = XFormula,
           TrData = Tr_Adult,TrFormula = TrFormula,
-           phyloTree = P,
-           distr="probit",
-           studyDesign=studyDesign,
-           ranLevels={list("transect" = rL.transect)})
+          phyloTree = P,
+          distr="probit",
+          studyDesign=studyDesign,
+          ranLevels={list("transect" = rL.transect)})
 
 models = list(m1)
 modelnames = c("presence_absence_ADULT")
 
 save(models,modelnames,file = "unfitted_models.Rdata")
-
